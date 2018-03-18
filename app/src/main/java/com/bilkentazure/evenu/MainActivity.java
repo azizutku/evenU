a02e1f1942607334e84580a3a71b836a07153046
Aysegul Gokce <aysegul.gokce@ug.bilkent.edu.tr>
2018-03-18 17:27:38 +0300

    Initial commit

--- app/src/main/java/com/bilkentazure/evenu/MainActivity.java
+++ app/src/main/java/com/bilkentazure/evenu/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//ddd
+		//ddddsadasdasda
 		//asdd
 	}
 }
